a12a39357ec3577c0e70f384766f125f71ef0fe1
Stephen M. Coakley <me@stephencoakley.com>
2016-12-02 14:09:43 -0600

    add test for accented characters
    DAM-8293

--- test/com/widen/util/InternalUtilsTest.java
+++ test/com/widen/util/InternalUtilsTest.java
@@ -13,5 +13,6 @@ public class InternalUtilsTest
 		assertEquals("+oo.jpg", InternalUtils.cleanAttachmentFilename("+ƒoo.jpg"));
 		assertEquals("foo.jpg", InternalUtils.cleanAttachmentFilename("𢃇𢞵𢫕foo𢭃𢯊𢱑𢱕.jpg"));
 		assertEquals("helloworld.jpg", InternalUtils.cleanAttachmentFilename("hello\nworld.jpg"));
+		assertEquals("resume.pdf", InternalUtils.cleanAttachmentFilename("résumé.pdf"));
 	}
 }
