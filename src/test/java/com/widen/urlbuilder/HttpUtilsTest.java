155409b578a269a481711033861310a3b04f9916
Uriah Carpenter <uriah@widen.com>
2026-03-06 11:50:01 -0600

    test: convert HttpUtilsTest to JUnit 5

--- src/test/java/com/widen/urlbuilder/HttpUtilsTest.java
+++ src/test/java/com/widen/urlbuilder/HttpUtilsTest.java
@@ -1,13 +1,13 @@
 package com.widen.urlbuilder;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class HttpUtilsTest
+class HttpUtilsTest
 {
     @Test
-    public void testCreateContentDispositionHeader()
+    void testCreateContentDispositionHeader()
     {
         assertEquals(
             "inline; filename=\"foo.jpg\"",
